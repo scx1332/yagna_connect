4df25466ab5d8751d81d5ad6188062b8222340d8
scx1332 <sieciech.czajka@golem.network>
2023-02-13 14:33:22 +0100

    Connect

---
+++ connect.py
@@ -0,0 +1,64 @@
+import asyncio
+import logging
+import aiohttp
+import platform
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+BEARER_TOKEN = "nhyujm"
+
+
+class PostException(Exception):
+    pass
+
+
+async def send_request(url, method="get", data=None):
+    headers = [
+        ('Content-Type', 'application/json'),
+        ('Accept', 'application/json'),
+        ('Authorization', f'Bearer {BEARER_TOKEN}')
+    ]
+    if data:
+        data_bytes = data.encode('utf-8')  # needs to be bytes
+        headers.append(('Content-Length', str(len(data_bytes))))
+    else:
+        data_bytes = None
+
+    try:
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.request(method, url, data=data_bytes) as result:
+                if result.status == 413:
+                    logger.error(
+                        f"Data exceeded RPC limit, data size {len(data_bytes)} try lowering batch size")
+                    raise PostException("Data too big")
+                if result.status == 401:
+                    logger.error(
+                        f"Unauthorized, check your API key, data size")
+                    raise PostException("Unauthorized")
+                if result.status != 200:
+                    logger.error(f"RPC call failed with status code {result.status}")
+                    raise PostException(f"Other error {result.status}")
+                try:
+                    content = await result.text()
+                    return content
+                except Exception as ex:
+                    logger.error(f"Error reading result {ex}")
+                    raise PostException(f"Error reading result {ex}")
+    except aiohttp.ClientConnectorError as ex:
+        logger.error(f"aiohttp.ClientConnectorError: {ex}")
+        raise PostException(f"aiohttp.ClientConnectorError: {ex}")
+
+
+async def main():
+    me_data = await send_request("http://127.0.0.1:7465/me")
+    print(f"Identity information: {me_data}")
+
+
+if __name__ == "__main__":
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(main())
