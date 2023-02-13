e83e669efc1f481a085d901434b5fbe027e68bb9
scx1332 <sieciech.czajka@golem.network>
2023-02-13 17:17:25 +0100

    Added way to generate providers

---
+++ provider/gen_env.py
@@ -0,0 +1,23 @@
+# Script for create .env file with random accounts
+# Look at README.md for more details
+
+import random
+import secrets
+import string
+from eth_account import Account
+
+for env_files in ["prov1.env", "prov2.env", "prov3.env"]:
+    with open(env_files, "w") as f:
+        random_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for _ in range(10))
+        f.write(f"# Yagna node name, don't have to be unique but it is nice to have\n")
+        f.write(f"NODE_NAME={random_name}\n")
+        private_key = "0x" + secrets.token_hex(32)
+        f.write(f"# Yagna private account\n")
+        f.write(f"YAGNA_AUTOCONF_ID_SECRET={private_key}\n")
+        f.write(f"# This one not needed, added for convenience\n")
+        f.write(f"YAGNA_AUTOCONF_PUBLIC_ADDRESS={Account.from_key(private_key).address}\n")
+
+        random_app_key = ''.join(random.choice(string.digits) for _ in range(20))
+        f.write(f"YAGNA_AUTOCONF_APPKEY={random_app_key}\n")
+        f.write(f"YAGNA_APPKEY={random_app_key}\n")
+
