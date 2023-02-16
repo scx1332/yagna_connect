c00999789613d5d541a3b1b3d7b3d02b3282754f
scx1332 <sieciech.czajka@golem.network>
2023-02-16 19:25:00 +0100

    Merging changes

--- connect.py
+++ connect.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import logging
 import shutil
@@ -16,7 +17,7 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-BEARER_TOKEN = "60385131821230470108"
+BEARER_TOKEN = ""
 SUBNET = "vpn"
 API_URL = "http://127.0.0.1:7465"
 API_URL_WEBSOCKETS = "ws://127.0.0.1:7465"
@@ -43,7 +44,7 @@ async def send_request(url, method="get", data=None):
     headers = [
         ('Content-Type', 'application/json'),
         ('Accept', 'application/json'),
-        ('Authorization', f'Bearer {BEARER_TOKEN}')
+        ('Authorization', f'Bearer {bearer_token}')
     ]
     if data:
         data_bytes = data.encode('utf-8')  # needs to be bytes
@@ -154,15 +155,33 @@ async def negotiate_agreement(sender_address):
                         .replace("%%SUBNET%%", SUBNET))
 
     dump_next_info("demand.json", json.dumps(demand, indent=4))
+
+    # Yagna daemon is automatically collecting Offers propagated by Providers.
+    # To find matching Providers we need to publish Demand on market, which will describe
+    # our needs. Market will later return events related to all Offers matching our Demand.
+    #
+    # Note: In opposition to Offers, our Demand is not propagated to other Nodes and will be visible
+    # only to those Nodes, that we start negotiations with.
     demand_id = await send_request(f"{API_URL}/market-api/v1/demands", method="post", data=json.dumps(demand, indent=4))
     demand_id = demand_id.replace('"', '')
+
     logger.info(f"Demands information: {demand_id}")
+    return demand_id, demand
+
+
+# This function will negotiate single Agreement with the first Provider that will respond
+# to us. In normal use cases probably we would like to have more advanced market strategy
+# which scores different Proposals based on the price requested in relation to resources offered.
+async def negotiate_agreement(sender_address):
+    demand_id, demand = await create_demand(sender_address)
 
     while True:
         max_events = 5
         poll_timeout = 3000
 
-        # Query market for new events (we are interested in new Proposals)
+        # Query market for new events (we are interested in new Proposals).
+        # Events are incoming asynchronously, since Providers' Offers are broadcasted through
+        # the network and aren't available immediately after we publish our Demand.
         events = await send_request(
             f"{API_URL}/market-api/v1/demands/{demand_id}/events?maxEvents={max_events}&pollTimeout={poll_timeout}")
         logger.info(f"Query result: {len(events)} event(s)")
@@ -205,9 +224,13 @@ async def negotiate_agreement(sender_address):
                     # In this case Provider responded to our first counter Proposal.
                     # We could try to propose Agreement.
                     proposal_id = received_proposal['proposalId']
+
+                    # `validTo` field specifies, how long will we wait for Agreement proposal acceptance.
+                    valid_to = datetime.now(timezone.utc) + timedelta(seconds=30)
+                    valid_to_formatted = valid_to.isoformat().replace("+00:00", "Z")
                     agreement_proposal = {
                         "proposalId": proposal_id,
-                        "validTo": demand_expiration_formatted_z
+                        "validTo": valid_to_formatted
                     }
 
                     logger.info(f"Creating Agreement for: {proposal_id}")
@@ -220,10 +243,19 @@ async def negotiate_agreement(sender_address):
                     await send_request(f"{API_URL}/market-api/v1/agreements/{agreement_id}/confirm", method="post",
                                        data=None)
 
+                    # We are waiting until Provide will accept our Agreement proposal.
+                    # Since Providers can negotiate with multiple Requestors and can implement different
+                    # market strategies, we can't be sure if our Agreement will be accepted.
+                    # This endpoint will return 410 (Gone) if Provider rejected Agreement proposal
+                    # and we need to go back to negotiations with other Providers.
                     logger.info(f"Waiting for Agreement: {agreement_id} Approval")
                     await send_request(f"{API_URL}/market-api/v1/agreements/{agreement_id}/wait",
                                        method="post", data=None)
                     logger.info(f"Agreement {agreement_id} approved")
+
+                    # We have signed Agreement and we are returning, without notifying other Providers
+                    # that we were negotiating with. It is correct behavior, but to be more gentle, we could
+                    # reject all other Proposals.
                     return agreement_id
                 else:
                     # Other states are unexpected, so continue the loop
@@ -236,7 +268,41 @@ async def negotiate_agreement(sender_address):
                 raise ex
 
 
+async def create_network():
+    # Here we will create new virtual network. Thanks to this Requestor and Providers
+    # will be able to communicate with each other using standard networking protocols (UDP, TCP).
+    # This step doesn't require signed Agreement, so it could be done anywhere in the code.
+    ip_remote = "192.168.8.7"
+    new_network = {
+        "ip": "192.168.8.0/24",
+        "mask": "255.255.255.0",
+        "gateway": ip_remote
+    }
+    # This call creates virtual network.
+    net_response = await send_request(f"{API_URL}/net-api/v2/vpn/net", method="post", data=json.dumps(new_network))
+    net_response = json.loads(net_response)
+    net_id = net_response["id"]
+
+    dump_next_info("net_response.json", json.dumps(net_response, indent=4))
+
+    # Assign Requestor ip address in created network.
+    ip_addr_resp = await send_request(f"{API_URL}/net-api/v2/vpn/net/{net_id}/addresses")
+    ip_local = json.loads(ip_addr_resp)
+    if ip_local[0]['ip'] != "192.168.8.1/24":
+        logger.error(f"Unexpected local ip {ip_local}")
+        raise Exception("Unexpected local ip {ip_local}")
+    ip_local = "192.168.8.1"
+
+    return net_response, ip_local, ip_remote
 async def main():
+    parser = argparse.ArgumentParser(
+        prog='ConnectVPN',
+        description='Simple demo script for outbound VPN activity')
+    parser.add_argument('--key')
+    global bearer_token
+    bearer_token = parser.parse_args().key
+
+
     await prepare_tmp_directory()
 
     me_data = await send_request(f"{API_URL}/me")
@@ -244,34 +310,37 @@ async def main():
     me_data = json.loads(me_data)
     sender_address = me_data["identity"]
 
+    # To compute anything, we need to sign Agreement with at least one Provider.
+    # This function implements whole negotiations process and returns negotiated Agreement.
     agreement_id = await negotiate_agreement(sender_address)
     logger.info(f"Agreement id successfully negotiated: {agreement_id}")
 
-    aggreement_resp = await send_request(f"{API_URL}/market-api/v1/agreements/{agreement_id}")
-    aggreement = json.loads(aggreement_resp)
-    provider_id = aggreement['offer']['providerId']
-    activity_request = {
-        "agreementId": agreement_id,
-        "requestorPubKey": None
-    }
-    activity = await send_request(f"{API_URL}/activity-api/v1/activity", method="post",
-                                  data=json.dumps(activity_request))
-    activity = json.loads(activity)
-    activity_id = activity['activityId']
-    logger.info(f"Activity id: {activity_id}")
-
     try:
-        ip_remote = "192.168.8.7"
-        new_network = {
-            "ip": "192.168.8.0/24",
-            "mask": "255.255.255.0",
-            "gateway": ip_remote
-        }
-        net_response = await send_request(f"{API_URL}/net-api/v2/vpn/net", method="post", data=json.dumps(new_network))
-        net_response = json.loads(net_response)
-        net_id = net_response["id"]
+        # Get content of the negotiated Agreement. This step is not required, since Agreement
+        # consists of final Proposals (Demand and Offer) from negotiation stage and we could
+        # already keep track of these information.
+        # Showing this method just for convenience.
+        aggreement_resp = await send_request(f"{API_URL}/market-api/v1/agreements/{agreement_id}")
+        aggreement = json.loads(aggreement_resp)
+        provider_id = aggreement['offer']['providerId']
 
-        dump_next_info("net_response.json", json.dumps(net_response, indent=4))
+        # Computations are done in the context of Activity, so we need to create one.
+        # Single Agreement can have multiple activities created after each other.
+        # Creating new activity will give you clear state of the ExeUnit Runtime.
+        # In most cases one activity will be enough to suite your needs.
+        activity_request = {
+            "agreementId": agreement_id,
+            "requestorPubKey": None
+        }
+        activity = await send_request(f"{API_URL}/activity-api/v1/activity", method="post",
+                                      data=json.dumps(activity_request))
+        activity = json.loads(activity)
+        activity_id = activity['activityId']
+        logger.info(f"Activity id: {activity_id}")
+
+        (network, local_ip, ip_remote) = await create_network()
+        net_id = network["id"]
+
 
         ip_addr_resp = await send_request(f"{API_URL}/net-api/v2/vpn/net/{net_id}/addresses")
         ip_local = json.loads(ip_addr_resp)
@@ -279,16 +348,20 @@ async def main():
             logger.error(f"Unexpected local ip {ip_local}")
             raise Exception("Unexpected local ip {ip_local}")
         ip_local = "192.168.8.1"
-
+        # Activity is created, now we need to start ExeUnit Runtime and initialize it.
+        # We do this by sending `Deploy` and `Start` commands. In `Deploy` command we specify
+        # network configuration.
         commands = [
             {
                 "deploy": {
                     "net": [
                         {
-                            "id": net_id,
-                            "ip": net_response["ip"],
-                            "mask": net_response["mask"],
-                            "nodeIp": ip_remote
+                            "id": network["id"],
+                            "ip": network["ip"],
+                            "mask": network["mask"],
+                            # Since we are initializing outbound gateway runtime we assign it
+                            # the same address as for default network gateway.
+                            "nodeIp": network["gateway"]
                         }
                     ]
                 }
@@ -305,6 +378,7 @@ async def main():
         logger.info(f"Deploying network on provider {net_id}")
         dump_next_info("exec_command.json", json.dumps(exec_command, indent=4))
 
+        # Execute commands in the context of created activity.
         response_exec = await send_request(f"{API_URL}/activity-api/v1/activity/{activity_id}/exec", method="post",
                                            data=json.dumps(exec_command))
         response_batch_id = response_exec.replace('"', '')
@@ -360,7 +434,7 @@ async def main():
         }
         if 1:
             async with websockets.connect(f"{API_URL_WEBSOCKETS}/net-api/v2/vpn/net/{net_id}/tcp/{ip_remote}/50671",
-                                          extra_headers=[('Authorization', f'Bearer {BEARER_TOKEN}')]) as websocket:
+                                          extra_headers=[('Authorization', f'Bearer {bearer_token}')]) as websocket:
                 logger.info(f"Connected to websocket")
                 while True:
                     await websocket.send("Hello")
@@ -375,6 +449,7 @@ async def main():
         traceback.print_exc(file=sys.stderr)
         logger.error(f"Error while sending activity events: {e}")
     finally:
+        # We should always free Provider, by terminating the Agreement.
         terminate_reason = {
             "message": "Finishing agreement",
             "extra": {}
@@ -382,6 +457,8 @@ async def main():
         terminate_agreement = await send_request(f"{API_URL}/market-api/v1/agreements/{agreement_id}/terminate",
                                                  method="post", data=json.dumps(terminate_reason))
         logger.info(f"Agreement terminated: {terminate_agreement}")
+
+        # Here we should wait for Invoice from Provider to accept.
 
 
 if __name__ == "__main__":
