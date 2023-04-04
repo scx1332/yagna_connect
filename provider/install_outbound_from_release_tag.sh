c0c34afbe4f65f436dd7b74620a98482d862a14a
scx1332 <sieciech.czajka@golem.network>
2023-04-04 10:50:20 +0200

    Better install scripts

--- provider/install_outbound_from_release_tag.sh
+++ provider/install_outbound_from_release_tag.sh
@@ -6,7 +6,7 @@ wget https://github.com/golemfactory/ya-runtime-outbound/releases/download/$1/ya
 wget https://github.com/golemfactory/ya-runtime-outbound/releases/download/$1/ya-runtime-outbound.json
 mkdir -p ~/.local/lib/yagna/plugins
 mkdir -p ~/.local/lib/yagna/plugins/ya-runtime-outbound
-cp ya-runtime-outbound.json ~/.local/lib/yagna/plugins/ya-runtime-outbound.json
-cp ya-runtime-outbound ~/.local/lib/yagna/plugins/ya-runtime-outbound/ya-runtime-outbound
+mv ya-runtime-outbound.json ~/.local/lib/yagna/plugins/ya-runtime-outbound.json
+mv ya-runtime-outbound ~/.local/lib/yagna/plugins/ya-runtime-outbound/ya-runtime-outbound
 cd ..
 rm tmp -r
--- provider/install_yagna_from_release_tag.sh
+++ provider/install_yagna_from_release_tag.sh
@@ -6,14 +6,14 @@ wget https://github.com/golemfactory/yagna/releases/download/$1/golem-provider-l
 tar -xzvf golem-provider-linux-$1.tar.gz
 rm golem-provider-linux-$1.tar.gz
 mkdir -p ~/.local/bin
-cp `find -name yagna` ~/.local/bin/yagna
-cp `find -name ya-provider` ~/.local/bin/ya-provider
-cp `find -name golemsp` ~/.local/bin/golemsp
+mv `find -name yagna` ~/.local/bin/yagna
+mv `find -name ya-provider` ~/.local/bin/ya-provider
+mv `find -name golemsp` ~/.local/bin/golemsp
 mkdir -p ~/.local/lib/yagna/plugins
-cp `find -name exe-unit` ~/.local/lib/yagna/plugins/exe-unit
+mv `find -name exe-unit` ~/.local/lib/yagna/plugins/exe-unit
 wget https://github.com/golemfactory/yagna/releases/download/$1/golem-requestor-linux-$1.tar.gz
 tar -xzvf golem-requestor-linux-$1.tar.gz
 rm golem-requestor-linux-$1.tar.gz
-cp `find -name gftp` ~/.local/bin/gftp
+mv `find -name gftp` ~/.local/bin/gftp
 cd ..
 rm tmp -r
