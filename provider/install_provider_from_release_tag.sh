d78a848d70d2889295dc2fd3e2a2954d0e9875e4
scx1332 <sieciech.czajka@golem.network>
2023-04-04 11:16:24 +0200

    improving docker build for provider

--- provider/install_provider_from_release_tag.sh
+++ provider/install_provider_from_release_tag.sh
@@ -2,18 +2,14 @@
 set -x
 mkdir tmp
 cd tmp
-wget https://github.com/golemfactory/yagna/releases/download/$1/golem-provider-linux-$1.tar.gz
-tar -xzvf golem-provider-linux-$1.tar.gz
-rm golem-provider-linux-$1.tar.gz
+wget -O- https://github.com/golemfactory/yagna/releases/download/$1/golem-provider-linux-$1.tar.gz | tar -xzf -
 mkdir -p ~/.local/bin
 mv `find -name yagna` ~/.local/bin/yagna
 mv `find -name ya-provider` ~/.local/bin/ya-provider
 mv `find -name golemsp` ~/.local/bin/golemsp
 mkdir -p ~/.local/lib/yagna/plugins
 mv `find -name exe-unit` ~/.local/lib/yagna/plugins/exe-unit
-wget https://github.com/golemfactory/yagna/releases/download/$1/golem-requestor-linux-$1.tar.gz
-tar -xzvf golem-requestor-linux-$1.tar.gz
-rm golem-requestor-linux-$1.tar.gz
+wget -O- https://github.com/golemfactory/yagna/releases/download/$1/golem-requestor-linux-$1.tar.gz | tar -xzf -
 mv `find -name gftp` ~/.local/bin/gftp
 cd ..
 rm tmp -r
